d582e9d97a6990077622c3854d8f1e2592dea750
Rodiah <mrodiah01@gmail.com>
2023-11-21 06:41:41 +0100

    Addition of new login activity

--- app/src/main/java/com/example/week5assessment/MainActivity.kt
+++ app/src/main/java/com/example/week5assessment/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.week5assessment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val loginButton: Button = findViewById(R.id.loginbutton)
+        loginButton.setOnClickListener{
+            val intent = Intent(this, Login_Page::class.java)
+        }
+
+
     }
+
 }
