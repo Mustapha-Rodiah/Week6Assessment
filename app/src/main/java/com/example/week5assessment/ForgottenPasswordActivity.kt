ab846a1d1a6fbf979a3a1748d1066d10eb0bdf5b
Rodiah <mrodiah01@gmail.com>
2023-11-26 08:29:33 +0100

    Addition of new Create Account activity, cange of Appp logo

--- app/src/main/java/com/example/week5assessment/ForgottenPasswordActivity.kt
+++ app/src/main/java/com/example/week5assessment/ForgottenPasswordActivity.kt
@@ -2,10 +2,29 @@ package com.example.week5assessment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar
 
 class ForgottenPasswordActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forgotten_password)
+        val email:EditText = findViewById(R.id.editText)
+var feedBack: String = email.text.toString()
+        val forgotPasswordBtn: MaterialButton = findViewById(R.id.forgotPasswordBtn)
+
+        forgotPasswordBtn.setOnClickListener{
+            var feedBack: String = email.text.toString()
+            var message = ""
+            val regex = "^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})"
+            if(!feedBack.matches(regex.toRegex())){
+            message = "Kindly enter a valid email address"
+            }
+            else {
+                message = "A recovery mail has been sent to ${feedBack}"
+            }
+            Snackbar.make(findViewById(R.id.forgotLayout), message, Snackbar.LENGTH_LONG).show()
+        }
     }
 }
--- app/src/main/java/com/example/week5assessment/MainActivity.kt
+++ app/src/main/java/com/example/week5assessment/MainActivity.kt
@@ -3,22 +3,50 @@ package com.example.week5assessment
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.Snackbar
+
 
 
 class MainActivity : AppCompatActivity() {
+    private fun showErrorMessage(message:String){
+        Snackbar.make(findViewById(R.id.secondPage), message, Snackbar.LENGTH_SHORT).show()
+
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+var loginButton:Button = findViewById(R.id.loginbutton)
+        var usernameInput: EditText = findViewById(R.id.username)
+        var passwordInput:EditText = findViewById(R.id.password)
+        loginButton.setOnClickListener{
+            var emailFeedBack = usernameInput.text.toString()
+            var passwordFeedBack = passwordInput.text.toString()
+            if(emailFeedBack.isEmpty() && passwordFeedBack.isEmpty()){
 
-val loginButton:Button = findViewById(R.id.loginbutton)
-        loginButton.setOnClickListener {
-            val intent = Intent(this, Login_Page::class.java)
+showErrorMessage("Username or Password can not be empty")
+            }
+            else {
+                val intent = Intent(this, Login_Page::class.java)
+                startActivity(intent)
+            }
+
+        }
+var forgotButton:Button = findViewById(R.id.forgotPassword)
+        forgotButton.setOnClickListener {
+            val intent = Intent(this, ForgottenPasswordActivity::class.java)
             startActivity(intent)
         }
 
-
+        var createAccount:Button = findViewById(R.id.createAccount)
+       createAccount.setOnClickListener {
+            val intent = Intent(this, CreateAccountActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
